6b8dcfd34656b5442b950faa4eb2ea1761d2c5e1
akvt227new@outlook.com <akvt227new@outlook.com>
2023-10-23 14:49:06 +0400

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using Shestb.Models;
+
+using System;
+using System.Diagnostics;
+using System.Net;
+
+namespace Shestb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            
+        }
+        [HttpPost]
+        public string Name(string NameText)
+        {
+            return "Спасибо, " + NameText + "!";
+        }
+        public string Square(int a = 15, int h = 10)
+        {
+            double s = a * h / 2.0;
+            return "<h2>Площадь треугольника с основанием " + a +
+                    " и высотой " + h + " равна " + s + "</h2>";
+        }
+
+        public string Squaree()
+        {
+            int a = Int32.Parse(Response.);
+            int h = 10;
+            double s = a * h / 2.0;
+            return "<h2>Площадь треугольника с основанием " + a + " и высотой " + h + " равна " + s + "</h2>";
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Shestb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
